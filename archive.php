f1179fbbc666460c35146b0dc7f50ad7fd70bffa
Md Robin Rana <developer.smrobin45@gmail.com>
2023-03-19 14:21:33 +0600

    added blog archive page in archive.php

--- archive.php
+++ archive.php
@@ -2,7 +2,8 @@
     <div class="page-banner">
         <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
         <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_archive_title();?></h1>
+        <h1 class="page-banner__title"><?php the_archive_title();?>
+        </h1>
         <div class="page-banner__intro">
             <p>Learn how the school of your dreams got started.</p>
         </div>

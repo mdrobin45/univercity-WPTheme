5552dc31e68a4609ad2da0143d5bc77852bc2829
Md Robin Rana <developer.smrobin45@gmail.com>
2023-05-22 10:35:17 +0600

    relationship subject

--- single-subject.php
+++ single-subject.php
@@ -32,6 +32,56 @@
                 </div>
                 <div class="generic-content">
                     <?php the_content();?>
+                    <?php
+                        // Custom query for event post type
+                        $today = date('Ymd');
+                        $args = array(
+                            'posts_per_page' => -1,
+                            'post_type' => 'event',
+                            'meta_key' => 'select_event_date',
+                            'orderby' => 'meta_value_num',
+                            'order' => 'ASC',
+                            'meta_query' => array(
+                                array(
+                                    'key' => 'select_event_date',
+                                    'compare' => '>=',
+                                    'value' => $today
+                                ),
+                                array(
+                                    'key' => "relational_subject",
+                                    'compare' => "LIKE",
+                                    'value' => '"'.get_the_ID().'"'
+                                )
+                            )
+                        );
+                        $custom_query = new WP_Query($args);
+                        if($custom_query->have_posts()){
+                            echo "<hr class='section-break'>";
+                            echo "<h2>Upcoming Events</h2>";
+                            // Event html structure
+                            if($custom_query -> have_posts()){
+                                while($custom_query -> have_posts()){
+                                    $custom_query -> the_post(); ?>
+                                    <div class="event-summary">
+                                        <?php
+                                            $event_date_string = get_field('select_event_date');
+                                            
+                                            $event_date = new DateTime($event_date_string);
+                                            
+                                        ?>
+                                        <a class="event-summary__date t-center" href="#">
+                                        <span class="event-summary__month"><?php echo $event_date->format('M');?></span>
+                                        <span class="event-summary__day"><?php echo $event_date->format('d');?></span>
+                                        </a>
+                                        <div class="event-summary__content">
+                                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h5>
+                                        <p><?php the_excerpt(); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                                        </div>
+                                    </div>
+                            <?php }
+                            }
+                        }
+                        ?>
                 </div>
             </div>
         <?php }
